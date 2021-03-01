c9656fb8cbec1700bcae95ce9583d4be3a2d67ec
Jerson Carin <63774442+jersoncarin@users.noreply.github.com>
2021-03-01 23:23:47 +0800

    Update index.js

--- functions/app/index.js
+++ functions/app/index.js
@@ -87,7 +87,7 @@ export default function expressApp(functionName) {
   app.use(morgan(customLogger))
 
   // Setup routes
-  app.use('/', router)
+  app.use(routerBasePath, router)
 
   // Apply express middlewares
   router.use(cors())
